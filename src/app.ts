9b80739d8f8265e8aec0ccb0148a235738d399dc
OLEkSandrKoniaiev <oleksandrkoniaiev.r@gmail.com>
2025-09-21 11:58:56 +0300

    feat: add app.ts, move app and morgan initialization to app.ts, create in app.ts basic route, install cors and mongoose libraries

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+// Підключаємо Morgan middleware залежно від середовища
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
+
+// Шляхи
+// app.use('/', ...);
+
+// Базовий запит
+app.get('/', (_req, res) => {
+  res.send('Hello World');
+});
+
+export default app;
